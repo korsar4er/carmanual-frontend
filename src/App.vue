40eb5d10a99caaa77cdc51286a0b105d52bc986c
Корсаков Антон Владимирович <korsakov_av@GT.NPO-SATURN.INT>
2020-06-04 16:51:44 +0300

    title template

--- src/App.vue
+++ src/App.vue
@@ -203,7 +203,7 @@ export default {
   },
   metaInfo: {
     title: "Инструкции по ремонту автомобилей",
-    titleTemplate: "PSA fix | %s",
+    titleTemplate: "RP | %s",
     meta: [
       // { "http-equiv": "Content-Type", content: "text/html; charset=utf-8" },
       // { name: "viewport", content: "width=device-width, initial-scale=1" },
